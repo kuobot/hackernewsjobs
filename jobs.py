87e09c4562c3728342f88700eaf90643706413d2
Joann Kuo <joannkuo@Joanns-MacBook-Air-3.local>
2018-03-28 11:28:52 -0700

    add .gitignore

---
+++ jobs.py
@@ -0,0 +1,23 @@
+# activities.py
+
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__, template_folder='.')
+
+@app.route('/')
+def homepage():
+ params = {
+   'api_key': 'tcGxTcvmgx6p'
+ }
+ print 'Getting results from parsehub'
+ r = requests.get(
+     'https://www.parsehub.com/api/v2/projects/tWdh5WjuNSPR/last_ready_run/data',
+     params=params)
+ print "Got results"
+ #print titles
+ return render_template('jobs.html', jobs=json.loads(r.text)['title'])
+
+if __name__ == '__main__':
+ app.run(debug=True)
